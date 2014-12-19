937c3e152d26c522c5a56244ec831d4948ddd9f1
Kioshi <Kioshi@a.b>
2014-12-19 19:16:46 +0100

    Moved creating meta-items and fluid-items to data-updates so we can be sure all other mod stuff is loaded
    THX again drs9999

---
+++ AlphaMod_0.0.3/data-updates.lua
@@ -0,0 +1,30 @@
+for k,prototype in pairs(data.raw["storage-tank"]) do
+    data:extend(
+    {
+        {
+            type = "item",
+            name = "AM-fluidbox-" .. prototype.name,
+            icon = "__base__/graphics/icons/deconstruction-planner.png",
+            flags = {"goes-to-main-inventory"},
+            fuel_value = prototype.fluid_box.base_area .. "J",
+            subgroup = "alphamod-metaitems",
+            order = "a",
+            stack_size = 1
+        },
+    })
+end
+
+for k,prototype in pairs(data.raw["fluid"]) do
+    data:extend(
+    {
+        {
+            type = "item",
+            name = prototype.name,
+            icon = prototype.icon,
+            flags = {"goes-to-main-inventory"},
+            subgroup = "logic-units",
+            order = "d[liquid-item]",
+            stack_size = 1
+        },
+    })
+end
--- AlphaMod_0.0.3/data.lua
+++ AlphaMod_0.0.3/data.lua
@@ -1,34 +1,3 @@
 require("prototypes.item")
 require("prototypes.recipe")
 require("prototypes.entity")
-
-for k,prototype in pairs(data.raw["storage-tank"]) do
-    data:extend(
-    {
-        {
-            type = "item",
-            name = "AM-fluidbox-" .. prototype.name,
-            icon = "__base__/graphics/icons/deconstruction-planner.png",
-            flags = {"goes-to-main-inventory"},
-            fuel_value = prototype.fluid_box.base_area .. "J",
-            subgroup = "alphamod-metaitems",
-            order = "a",
-            stack_size = 1
-        },
-    })
-end
-
-for k,prototype in pairs(data.raw["fluid"]) do
-    data:extend(
-    {
-        {
-            type = "item",
-            name = prototype.name,
-            icon = prototype.icon,
-            flags = {"goes-to-main-inventory"},
-            subgroup = "logic-units",
-            order = "d[liquid-item]",
-            stack_size = 1
-        },
-    })
-end
