51c562aa45401bdbf2b3f5adaf6939b507125513
Kioshi <Kioshi@a.b>
2014-12-31 15:28:42 +0100

    Trash Bin removed useless gui element

--- AlphaMod_0.1.0/scripts/player/trash-bin.lua
+++ AlphaMod_0.1.0/scripts/player/trash-bin.lua
@@ -13,10 +13,9 @@ function TrashBin.OnSelfOpen(player, index)
         player.gui.top.AmTbGUI.destroy()
     end
     
-    local frame = player.gui.top.add({ type="frame", direction="horizontal", name="AmTbGUI" })
-    local flow = frame.add({ type="flow", direction="vertical" })
-    flow.add({ type="label", caption = {"player-trash-bin"} })
-    flow.add({ type="button", name="AmTbGUIButton", style="not_available_slot_button_style"})
+    local frame = player.gui.top.add({ type="frame", direction="vertical", name="AmTbGUI" })
+    frame.add({ type="label", name="AmTbGUILabel", caption = {"player-trash-bin"} })
+    frame.add({ type="button", name="AmTbGUIButton", style="not_available_slot_button_style"})
 
 end
 
