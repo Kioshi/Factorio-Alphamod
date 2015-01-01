35dff47f110a69dfead55cdfb10cdb9d04a3e2e1
Kioshi <Kioshi@a.b>
2015-01-01 23:11:05 +0100

    Revert "Added fixed pipe compatibility with other mods pipes"
    This reverts commit 3f8732cdb908c07e340bd4bbdebde5db36c4d524.
    
    Conflicts:
    	AlphaMod_0.1.1/data-updates.lua
    	AlphaMod_0.1.1/scripts/pipes/pipes_fast_replace.lua

--- AlphaMod_0.1.1/scripts/pipes/pipes_fast_replace.lua
+++ AlphaMod_0.1.1/scripts/pipes/pipes_fast_replace.lua
@@ -3,13 +3,9 @@ FastReplacePipes = {}
 EventHandler.Register(FastReplacePipes) 
 
 function FastReplacePipes.OnEntityBuild(event)
-    if (glob.AlphaMod == nil) or (glob.AlphaMod.FixedPipes == nil) then
-        return
-    end
-
 	for playerIndex,player in pairs(game.players) do
 		if (player ~= nil) and (player.cursorstack ~= nil) then
-            if (fncINtable(player.cursorstack.name,glob.AlphaMod.FixedPipes) == true) then
+            if (fncIN(player.cursorstack.name,"pipe-l","pipe-t","pipe-i") == true) then
                 game.player.rotateforbuild()
                 game.player.rotateforbuild()
             end
@@ -17,19 +13,4 @@ function FastReplacePipes.OnEntityBuild(event)
 	end
 end
 
-function FastReplacePipes.LoadFixedPipes()
-    CreateGlobalTable("FixedPipes")
-    for index, item in pairs(game.itemprototypes) do
-        if (string.match(index, '-[ilt]$') ~= nil) then
-            glob.AlphaMod.FixedPipes[index] = index
-        end
-    end
-end
 
-function FastReplacePipes.OnInit()
-    FastReplacePipes.LoadFixedPipes()
-end
-
-function FastReplacePipes.OnLoad()
-    FastReplacePipes.LoadFixedPipes()
-end
